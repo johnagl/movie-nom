4b2f7ad8c6fd64c3fd0c324ea6c2b19637018a3a
John Aguilar <42220961+johnagl@users.noreply.github.com>
2021-01-05 15:03:29 -0800

    Initial commit, implement movie search/nominations tabs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-function App() {
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
     </div>
   );
 }
 
-export default App;
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  tabs: {
+    margin: "auto"
+  },
+  tab: {
+    minWidth: "20em"
+  }
+}));
+
+export default function App() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Tabs className={classes.tabs} value={value} onChange={handleChange}>
+            <Tab className={classes.tab} label="Movie Search" />
+            <Tab className={classes.tab} label="Nominations"  />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={value} index={0}>
+          Movie Search
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Nominations
+        </TabPanel>
+      </div>
+  );
+}
